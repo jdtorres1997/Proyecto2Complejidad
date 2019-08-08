2a8f8f06800ae6f9afb84ff2b4932335073d0c85
Carlos Tovar <cartovarc@gmail.com>
2019-08-08 00:09:27 -0500

    beautiful output :

--- GenericNewspaperGUI.py
+++ GenericNewspaperGUI.py
@@ -15,7 +15,6 @@ solver_name = "Gecode"
 
 window = Tk()
 window.title("Proyect part #2")
-window.geometry('730x400')
 
 # declare labels
 topic_label = Label(window, text="Topic")
@@ -38,7 +37,7 @@ max_pages_input = Entry(window,width=10)
 potential_readers_per_page_input = Entry(window,width=10)
 pages_number_input = Entry(window,width=10)
 
-#inputs positions
+# inputs positions
 topic_input.grid(sticky="W", column=2, row=0)
 min_pages_input.grid(sticky="W", column=2, row=1)
 max_pages_input.grid(sticky="W", column=2, row=2)
@@ -48,11 +47,13 @@ pages_number_input.grid(sticky="W", column=1, row=5)
 listbox = Listbox(window,width=120)
 listbox.grid(column=0, row=6, columnspan=3)
 
+
 def clean_inputs():
     topic_input.delete(first=0, last=len(topic_input.get()))
     min_pages_input.delete(first=0, last=len(min_pages_input.get()))
     max_pages_input.delete(first=0, last=len(max_pages_input.get()))
     potential_readers_per_page_input.delete(first=0, last=len(potential_readers_per_page_input.get()))
+
 
 def add_entry_clicked():
     topic = topic_input.get()
@@ -82,6 +83,7 @@ def add_entry_clicked():
     entries[naive_key] = entry_object
     clean_inputs()
 
+
 def create_data_file():
     output_file = open(data_output_filename, "w")
     output_file.write("% Archivo de datos\n")
@@ -93,6 +95,7 @@ def create_data_file():
         table_data = table_data + entry_str
     output_file.write("tabla=[%s];\n" % table_data)
     output_file.close()
+
 
 def solve_problem_with_current_entries():
 
@@ -111,22 +114,28 @@ def solve_problem_with_current_entries():
         return
 
     create_data_file()
-    process = subprocess.Popen("minizinc --solver %s %s %s"%(solver_name, model_filename, data_output_filename), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process = subprocess.Popen("minizinc --solver %s %s %s --output-mode json"%(solver_name, model_filename, data_output_filename), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     result_process = process.stdout.read()
-    #TODO make a friendly showinfo with result_process
-    messagebox.showinfo("Periodic problem solution", result_process)
+    result_json_str = result_process[:-24] # remove useless characters
+    result_dir = json.loads(result_json_str)
+    solution_str = ""
+    for index, (key, entry) in enumerate(entries.items()):
+        solution_str = solution_str + "Topic: %s, Pages: %s, Selected: %s\n"%(entry["topic"], result_dir["paginas"][index], "YES" if result_dir["seleccion"][index] else "NO")
+    messagebox.showinfo("Newspaper problem solution", solution_str)
+
 
 def delete_entry(entry_str):
     entry_str = entry_str.replace("'", '"') # use double quote
     entry_object = json.loads(entry_str)
     entries.pop(entry_object["pk"], None) # delete entry from entries
 
-#declare buttons
+
+# declare buttons
 add_entry_btn = Button(window, text="Add entry", command=add_entry_clicked)
 solve_problem_with_current_entries_btn = Button(window, text="Solve problem for all entries", command=solve_problem_with_current_entries)
-delete_entry_btn = Button(window, text = "Delete entry", command = lambda listbox=listbox: (delete_entry(listbox.get(ACTIVE)), listbox.delete(ANCHOR)))
+delete_entry_btn = Button(window, text = "Delete entry", command = lambda listbox=listbox: (delete_entry(listbox.get(ACTIVE)), listbox.delete(ACTIVE)))
 
-#buttons positions
+# buttons positions
 add_entry_btn.grid(sticky="W", column=2, row=4)
 solve_problem_with_current_entries_btn.grid(column=1, row=5)
 delete_entry_btn.grid(column=0, row=7, columnspan=3)
